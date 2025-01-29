fb2a75eb9c3f7efe175ed49a2e512bad9eff7e3a
amansingh454545 <cutekavya583@gmail.com>
2025-01-29 17:24:18 +0000

    Add files via upload

---
+++ server_TLS.c
@@ -0,0 +1,256 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+
+#define MAX_CLIENTS 100
+#define BUFFER_SZ 2048
+
+static _Atomic unsigned int cli_count = 0;
+static int uid = 10;
+
+/* SSL Context */
+SSL_CTX *ssl_ctx;
+
+/* Client structure */
+typedef struct {
+    struct sockaddr_in address;
+    int sockfd;
+    int uid;
+    char name[32];
+    SSL *ssl;
+} client_t;
+
+client_t *clients[MAX_CLIENTS];
+
+pthread_mutex_t clients_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+/* Initialize SSL */
+SSL_CTX *init_ssl() {
+    SSL_library_init();
+    OpenSSL_add_all_algorithms();
+    SSL_load_error_strings();
+    SSL_CTX *ctx = SSL_CTX_new(TLS_server_method());
+
+    if (!ctx) {
+        perror("Failed to create SSL context");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Load certificate and key */
+    if (SSL_CTX_use_certificate_file(ctx, "server_cert.pem", SSL_FILETYPE_PEM) <= 0 ||
+        SSL_CTX_use_PrivateKey_file(ctx, "server_key.pem", SSL_FILETYPE_PEM) <= 0) {
+        perror("Failed to load certificate or key");
+        exit(EXIT_FAILURE);
+    }
+    
+    return ctx;
+}
+
+void str_overwrite_stdout() {
+    printf("\r%s", "> ");
+    fflush(stdout);
+}
+
+void str_trim_lf(char *arr, int length) {
+    for (int i = 0; i < length; i++) {
+        if (arr[i] == '\n') {
+            arr[i] = '\0';
+            break;
+        }
+    }
+}
+
+void print_client_addr(struct sockaddr_in addr) {
+    printf("%d.%d.%d.%d",
+           addr.sin_addr.s_addr & 0xff,
+           (addr.sin_addr.s_addr & 0xff00) >> 8,
+           (addr.sin_addr.s_addr & 0xff0000) >> 16,
+           (addr.sin_addr.s_addr & 0xff000000) >> 24);
+}
+
+/* Add clients to queue */
+void queue_add(client_t *cl) {
+    pthread_mutex_lock(&clients_mutex);
+
+    for (int i = 0; i < MAX_CLIENTS; ++i) {
+        if (!clients[i]) {
+            clients[i] = cl;
+            break;
+        }
+    }
+
+    pthread_mutex_unlock(&clients_mutex);
+}
+
+/* Remove clients from queue */
+void queue_remove(int uid) {
+    pthread_mutex_lock(&clients_mutex);
+
+    for (int i = 0; i < MAX_CLIENTS; ++i) {
+        if (clients[i] && clients[i]->uid == uid) {
+            clients[i] = NULL;
+            break;
+        }
+    }
+
+    pthread_mutex_unlock(&clients_mutex);
+}
+
+/* Send message to all clients except sender */
+void send_message(char *s, int uid) {
+    pthread_mutex_lock(&clients_mutex);
+
+    for (int i = 0; i < MAX_CLIENTS; ++i) {
+        if (clients[i] && clients[i]->uid != uid) {
+            if (SSL_write(clients[i]->ssl, s, strlen(s)) <= 0) {
+                perror("ERROR: SSL write failed");
+                break;
+            }
+        }
+    }
+
+    pthread_mutex_unlock(&clients_mutex);
+}
+
+/* Handle all communication with the client */
+void *handle_client(void *arg) {
+    char buff_out[BUFFER_SZ];
+    char name[32];
+    int leave_flag = 0;
+
+    cli_count++;
+    client_t *cli = (client_t *)arg;
+
+    /* Perform SSL handshake */
+    if (SSL_accept(cli->ssl) <= 0) {
+        perror("SSL handshake failed");
+        leave_flag = 1;
+    }
+
+    /* Name */
+    if (!leave_flag && SSL_read(cli->ssl, name, 32) <= 0) {
+        printf("Didn't enter the name.\n");
+        leave_flag = 1;
+    } else {
+        strcpy(cli->name, name);
+        sprintf(buff_out, "%s has joined\n", cli->name);
+        printf("%s", buff_out);
+        send_message(buff_out, cli->uid);
+    }
+
+    while (!leave_flag) {
+        int receive = SSL_read(cli->ssl, buff_out, BUFFER_SZ);
+        if (receive > 0) {
+            buff_out[receive] = '\0';
+            send_message(buff_out, cli->uid);
+            str_trim_lf(buff_out, strlen(buff_out));
+            printf("%s -> %s\n", buff_out, cli->name);
+        } else {
+            sprintf(buff_out, "%s has left\n", cli->name);
+            printf("%s", buff_out);
+            send_message(buff_out, cli->uid);
+            leave_flag = 1;
+        }
+
+        memset(buff_out, 0, BUFFER_SZ);
+    }
+
+    /* Cleanup */
+    close(cli->sockfd);
+    SSL_free(cli->ssl);
+    queue_remove(cli->uid);
+    free(cli);
+    cli_count--;
+    pthread_detach(pthread_self());
+
+    return NULL;
+}
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        printf("Usage: %s <port>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    char *ip = "127.0.0.1";
+    int port = atoi(argv[1]);
+    int option = 1;
+    int listenfd = 0, connfd = 0;
+    struct sockaddr_in serv_addr;
+    struct sockaddr_in cli_addr;
+    pthread_t tid;
+
+    /* Initialize SSL */
+    ssl_ctx = init_ssl();
+
+    /* Socket settings */
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr(ip);
+    serv_addr.sin_port = htons(port);
+
+    signal(SIGPIPE, SIG_IGN);
+
+    if (setsockopt(listenfd, SOL_SOCKET, (SO_REUSEPORT | SO_REUSEADDR), (char *)&option, sizeof(option)) < 0) {
+        perror("ERROR: setsockopt failed");
+        return EXIT_FAILURE;
+    }
+
+    /* Bind */
+    if (bind(listenfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("ERROR: Socket binding failed");
+        return EXIT_FAILURE;
+    }
+
+    /* Listen */
+    if (listen(listenfd, 10) < 0) {
+        perror("ERROR: Socket listening failed");
+        return EXIT_FAILURE;
+    }
+
+    printf("=== SECURE CHATROOM ===\n");
+
+    while (1) {
+        socklen_t clilen = sizeof(cli_addr);
+        connfd = accept(listenfd, (struct sockaddr *)&cli_addr, &clilen);
+
+        if ((cli_count + 1) == MAX_CLIENTS) {
+            printf("Max clients reached. Rejected: ");
+            print_client_addr(cli_addr);
+            printf(":%d\n", cli_addr.sin_port);
+            close(connfd);
+            continue;
+        }
+
+        /* Create SSL object */
+        SSL *ssl = SSL_new(ssl_ctx);
+        SSL_set_fd(ssl, connfd);
+
+        /* Client settings */
+        client_t *cli = (client_t *)malloc(sizeof(client_t));
+        cli->address = cli_addr;
+        cli->sockfd = connfd;
+        cli->uid = uid++;
+        cli->ssl = ssl;
+
+        /* Add client to queue and fork thread */
+        queue_add(cli);
+        pthread_create(&tid, NULL, &handle_client, (void *)cli);
+
+        sleep(1);
+    }
+
+    close(listenfd);
+    SSL_CTX_free(ssl_ctx);
+    return EXIT_SUCCESS;
+}
